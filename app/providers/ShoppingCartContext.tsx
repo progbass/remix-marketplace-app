e3b7e3fd4f6422fcf3be1f7a7c99bdfe5e7d0d55
Israel Díaz <progbass@gmail.com>
2024-05-29 00:09:40 -0600

    update ShoppingCartProvider when parent properties change

--- app/providers/ShoppingCartContext.tsx
+++ app/providers/ShoppingCartContext.tsx
@@ -1,5 +1,12 @@
 // ShoppingCartContext.js
-import { useState, useEffect, createContext, useContext, useReducer } from "react";
+import {
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+  useReducer,
+  memo,
+} from "react";
 
 import type { ShoppingCartType } from "~/utils/ShoppingCart";
 import ShoppingCart from "~/utils/ShoppingCart";
@@ -7,29 +14,34 @@ import ShoppingCart from "~/utils/ShoppingCart";
 const ShoppingCartInstance = new ShoppingCart();
 const ShoppingCartContext = createContext(ShoppingCartInstance);
 
-export const ShoppingCartProvider = (
-  { children, items } : {children: any, items: ShoppingCartType}
-) => {
-  const [cart, setCart] = useState(new ShoppingCart(items));
+export const ShoppingCartProvider = memo(
+  ({ children, items }: { children: any; items: ShoppingCartType }) => {
+    const [cart, setCart] = useState(new ShoppingCart(items));
 
-  // Subscribe to changes
-  useEffect(() => {
-    const unsubscribe = cart.subscribe((currentCart: ShoppingCartType) => {
-      console.log('-------------> Context updating: ', currentCart);
-      // cart = undefined;
-      setCart(new ShoppingCart(currentCart));
-    });
+    // Subscribe to changes
+    useEffect(() => {
+      const unsubscribe = cart.subscribe((currentCart: ShoppingCartType) => {
+        console.log("-------------> Context updating: ", currentCart);
+        // cart = undefined;
+        setCart(new ShoppingCart(currentCart));
+      });
 
-    // return () => unsubscribe();
-  }, [cart]);
+      // return () => unsubscribe();
+    }, [cart]);
 
-  // Return the provider
-  return (
-    <ShoppingCartContext.Provider value={cart}>
-      {children}
-    </ShoppingCartContext.Provider>
-  );
-};
+    // Update state if parent props change
+    useEffect(() => {
+      setCart(new ShoppingCart(items));
+    }, [items]);
+
+    // Return the provider
+    return (
+      <ShoppingCartContext.Provider value={cart}>
+        {children}
+      </ShoppingCartContext.Provider>
+    );
+  }
+);
 
 // Export
 export { ShoppingCartContext };
