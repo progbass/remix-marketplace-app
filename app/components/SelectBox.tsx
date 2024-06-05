2e801bdfdde24465bbbbd37e78e6b4997f14e0f3
Israel Díaz <progbass@gmail.com>
2024-06-04 19:49:27 -0600

    Updates to SelectBox component

--- app/components/SelectBox.tsx
+++ app/components/SelectBox.tsx
@@ -45,38 +45,40 @@ export default function SelectBox({
 }: SelectBoxProps) {
   const uniqueId = name || uuidv4();
 
-  // Selected value state
-  const [selected, setSelected] = useState<Option>(value ?? DEFAULT_OPTION);
-  useEffect(() => {
-    // When the parent sets value to null, reset the internal state
-    if (value === null) {
-      setSelected(DEFAULT_OPTION);
-      return;
-    }
-    setSelected(value);
-  }, [value]);
-
-  // Handle change
-  const onValueChange = (selectedOption: {
-    name: string;
-    id: string | number;
-  }) => {
-    if (onChange) {
-      onChange(selectedOption);
-    }
-    setSelected(selectedOption);
-  };
-
   // Add default option to the list
   let finalOptionsList = optionsList;
   if(!excludeDefaultOption) {
     finalOptionsList = [DEFAULT_OPTION, ...finalOptionsList];
   }
+
+  // Selected value state
+  const [selectedOption, setSelectedOption] = useState<Option>(value ?? finalOptionsList[0]);
+  useEffect(() => {
+    // When the parent sets value to null, reset the internal state
+    if (value === null) {
+      setSelectedOption(finalOptionsList[0]);
+      return;
+    }
+    setSelectedOption(value);
+  }, [value]);
+
+  // Handle change
+  const onValueChange = (option: {
+    name: string;
+    id: string | number;
+  }) => {
+    if (onChange) {
+      onChange(option);
+    }
+    setSelectedOption(option);
+  };
+
+  console.log('finalOptionsList', selectedOption);
   
   // Return JSX
   return (
     <>
-      <Listbox disabled={disabled} value={selected} onChange={onValueChange}>
+      <Listbox disabled={disabled} value={selectedOption} onChange={onValueChange}>
         {({ open }) => (
           <>
             
@@ -92,8 +94,8 @@ export default function SelectBox({
             )}
 
             <div className="relative mt-2">
-              <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                <span className="block truncate">{selected ? selected.name : DEFAULT_OPTION.name}</span>
+              <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-secondary-600 sm:text-sm sm:leading-6">
+                <span className="block truncate">{ selectedOption.name }</span>
                 <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                   <ChevronUpDownIcon
                     className="h-5 w-5 text-gray-400"
@@ -109,7 +111,9 @@ export default function SelectBox({
                 leaveFrom="opacity-100"
                 leaveTo="opacity-0"
               >
-                <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                <Listbox.Options 
+                  className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                >
                   {finalOptionsList.map(
                     (option: { id: string | number | null; name: string }) => (
                       <Listbox.Option
@@ -117,7 +121,7 @@ export default function SelectBox({
                         className={({ active }) =>
                           classNames(
                             active
-                              ? "bg-indigo-600 text-white"
+                              ? "bg-gray-300 text-white"
                               : "text-gray-900",
                             "relative cursor-default select-none py-2 pl-3 pr-9"
                           )
@@ -138,7 +142,7 @@ export default function SelectBox({
                             {selected ? (
                               <span
                                 className={classNames(
-                                  active ? "text-white" : "text-indigo-600",
+                                  active ? "text-white" : "text-secondary-600",
                                   "absolute inset-y-0 right-0 flex items-center pr-4"
                                 )}
                               >
